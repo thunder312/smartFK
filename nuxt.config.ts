9a7d62518ded24baa3f51785dd1c484cae0da617
Daniel Ertl <dertl@web.de>
2026-02-25 16:07:58 +0100

    final checkin. Moved to sos repository

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  rootDir: new URL('.', import.meta.url).pathname,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
